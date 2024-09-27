950ae61c3feceaafd1e0af4656611d5e3e887e3d
deimencart <diego.mencart@gmail.com>
2024-09-26 17:02:23 -0700

    Create input_output.py

---
+++ Inputs and outputs/input_output.py
@@ -0,0 +1,9 @@
+#Exercise Day1 
+#Exercise to exercise inputs and outputs
+
+# 09/23/24
+
+print("Hello, Welcome to the band Generator")
+city = input("What is the name of the city you grew up in?:  ")
+name_animal = input("what was your dogs name?: ")
+print ("Your band name is: " + city + " " + name_animal)
